433759c083d4a2417544fe79a471c986f2d8425a
MShriDivya <krdivyashri@gmail.com>
2017-06-20 15:15:11 +0530

    Create Alpha.java

---
+++ Alpha.java
@@ -0,0 +1,30 @@
+ import java.util.*;
+import java.lang.*;
+import java.io.*;
+    public class Alpha
+
+    {
+
+        public static void main(String[] args) throws Exception 
+
+        {
+
+            char ch;
+            Scanner s=new Scanner(System.in);
+          
+
+            System.out.print("Enter the character you want to check:");
+            ch=s.next().charAt(0);
+            if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) 
+            {
+            	System.out.println("the character "+ch+" is alphabet");
+            }
+            else
+            {
+            	System.out.print("the character "+ch+" is not an alphabet");
+            }
+           
+
+        }
+
+    }
