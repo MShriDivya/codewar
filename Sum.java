ab44c28a18380fb3507021132a22692986b512d8
MShriDivya <krdivyashri@gmail.com>
2017-06-20 16:15:11 +0530

    Create Sum.java

---
+++ Sum.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Sum
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int a=20;
+		int b=30;
+		
+		system.out.print(a+b);
+		
+		
+		}
+}
