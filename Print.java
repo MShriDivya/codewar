1d8ab9215ba2a5c5254f7bf7f8bc2cf066a834f0
MShriDivya <krdivyashri@gmail.com>
2017-06-20 16:13:46 +0530

    Create Print.java

---
+++ Print.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Print {
+public static void main(String args[])
+{
+	int i;
+for(i=0;i<=5;i++)
+{
+	System.out.println("Hello world");
+}
+}
+}
