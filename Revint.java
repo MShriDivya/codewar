1c4e43e2464cadcc43bb00f3a544343719ea489c
MShriDivya <krdivyashri@gmail.com>
2017-06-22 09:55:11 +0530

    Create Revint.java

---
+++ Revint.java
@@ -0,0 +1,24 @@
+
+
+import java.util.Scanner;
+ 
+class Revint
+{
+   public static void main(String args[])
+   {
+      int n, r = 0;
+ 
+      System.out.println("Enter the number ");
+      Scanner in = new Scanner(System.in);
+      n = in.nextInt();
+ 
+      while( n != 0 )
+      {
+          r = r * 10;
+          r = r + n%10;
+          n = n/10;
+      }
+ 
+      System.out.println("Reverse  number  "+r);
+   }
+}
