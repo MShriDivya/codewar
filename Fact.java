3108639d3047580b5841c18cb7d8e53ac6f41ea4
MShriDivya <krdivyashri@gmail.com>
2017-06-23 10:22:00 +0530

    Create Fact.java

---
+++ Fact.java
@@ -0,0 +1,20 @@
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+
+class Fact
+{
+    public static void main(String[] args)
+    {
+    	int f=1;
+    	int a;
+   	 Scanner s=new Scanner(System.in);
+        System.out.println("enter number");
+        a=s.nextInt();
+        for(int i=1;i<=a;i++)
+        {
+        f= f*i;
+       
+        }
+        System.out.println("factorial "  +f);
+      }
