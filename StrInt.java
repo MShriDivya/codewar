07214f0db124200a3c21c3fb67aa84fb06e238b6
MShriDivya <krdivyashri@gmail.com>
2017-06-23 12:18:18 +0530

    Update StrInt.java

--- StrInt.java
+++ StrInt.java
@@ -1,7 +1,7 @@
 public class strtoint{  
 public static void main(String args[]){  
-String s="200";  
-int i=Integer.parseInt(s);  
+String mn="500";  
+int i=Integer.parseInt(mn);  
 System.out.println(s+100);  
 System.out.println(i+100); 
 }}  
